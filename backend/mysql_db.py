a65424a73f82dbaace1829a54614be2ac0295e9d
Sakshi Bute <sakshibute44@gmail.com>
2026-02-26 09:19:33 +0530

    Update mysql_db.py

--- backend/mysql_db.py
+++ backend/mysql_db.py
@@ -4,7 +4,7 @@ def get_db_connection():
     connection = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="sakshi",
+        password="Add_Password",
         database="microproject_db"
     )
     return connection
